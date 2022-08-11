9dc0204ad8987806270e8184bbaca088dfdf3da4
veoco <one@nomox.cn>
2022-08-11 09:01:26 +0800

    add LibreSpeed support

--- src/components/server_item.jsx
+++ src/components/server_item.jsx
@@ -1,8 +1,18 @@
 const ServerItem = ({ item }) => {
+  let name, host, cc;
+  if(item.provider == "Ookla"){
+    name = `${item.detail.sponsor} - ${item.detail.name}`;
+    host = item.detail.host;
+    cc = ' · ' + item.detail.cc
+  } else if (item.provider == "LibreSpeed"){
+    name = `${item.detail.sponsorName} - ${item.detail.name}`;
+    host = item.detail.server;
+    cc = "";
+  }
   return (
     <div className="my-2 border border-gray-700 bg-white p-2 last:mb-0">
-      <h3><span className="before:content-['#'] px-1 mr-2 bg-stone-200 text-gray-700">{item.pk}</span>{item.detail.name} - {item.detail.sponsor}</h3>
-      <p className="text-gray-400">{item.provider} - {item.detail.id} · {item.detail.cc} - {item.detail.host}</p>
+      <h3><span className="before:content-['#'] px-1 mr-2 bg-stone-200 text-gray-700">{item.pk}</span>{name}</h3>
+      <p className="text-gray-400">{item.provider} - {item.detail.id}{cc} - {host}</p>
     </div>
   )
 }
