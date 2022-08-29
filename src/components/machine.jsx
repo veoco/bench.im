11fc89c670da3c6b3df9009786c1bfd4f48599fb
veoco <one@nomox.cn>
2022-08-29 09:07:34 +0800

    create task from machine page

--- src/components/machine.jsx
+++ src/components/machine.jsx
@@ -1,14 +1,16 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams, Link } from 'react-router-dom';
 import { FormattedMessage } from "react-intl";
 import useSWR from 'swr'
 
 import MachineItem from "./machine_item";
 import MachineTaskItem from "./machine_task_item";
+import MachineTaskCreate from "./machine_task_create";
 
 const Machine = () => {
   const { uuid } = useParams();
   const { data, error } = useSWR(`/api/machine/?pk=${uuid}`);
+  const [show, setShow] = useState(false)
 
   useEffect(() => {
     document.title = `Machine - Bench.im`;
@@ -23,7 +25,11 @@ const Machine = () => {
   return (
     <div className="mx-auto sm:w-2/5 py-2 text-justify">
       <MachineItem item={data} />
-      <h3><FormattedMessage defaultMessage="Machine Tasks" /> <Link className="text-sm float-right bg-white w-5 text-center border border-gray-700" to={`/machine/${uuid}/task/`}>+</Link></h3>
+      <h3>
+        <FormattedMessage defaultMessage="Machine Tasks" />
+        <button className="text-sm float-right bg-white w-5 text-center border border-gray-700" onClick={() => { setShow(!show) }}>+</button>
+      </h3>
+      {show ? <MachineTaskCreate uuid={uuid} setShow={setShow} /> : ""}
       {data.tasks.map((item) => {
         return (
           <MachineTaskItem item={item} key={item.pk} />
--- src/components/machine_task_create.jsx
+++ src/components/machine_task_create.jsx
@@ -1,76 +1,24 @@
-import { useState, useEffect } from "react";
-import { useSearchParams, useParams, useNavigate, Link } from "react-router-dom";
-import useSWR from 'swr'
+import { useState } from "react";
 import { FormattedMessage } from "react-intl";
+import { useSWRConfig } from 'swr'
 
 
-const MachineTaskCreate = () => {
-  const { uuid } = useParams();
-  const [searchParams, setSearchParams] = useSearchParams();
+const MachineTaskCreate = ({ uuid, setShow }) => {
+  const { mutate } = useSWRConfig()
 
   const [machineId, setMachineId] = useState(uuid);
-  const [state, setState] = useState("Wait");
-  const [oneshot, setOneshot] = useState(false);
   const [ipv6, setIpv6] = useState(false);
   const [thread, setThread] = useState(1);
   const [serverId, setServerId] = useState('');
-
-  const navigate = useNavigate();
-
-  const logined = new Date(localStorage.getItem('logined'));
-  const now = new Date();
-  let isLogin = true;
-  if ((now - logined) > 14 * 86400000) {
-    isLogin = false;
-  }
-
-  useEffect(() => {
-    if (!isLogin) {
-      navigate("/login/");
-    }
-
-    if (searchParams.get("pk")) {
-      document.title = `Edit machine task ${searchParams.get("pk")} - Bench.im`;
-    } else {
-      document.title = `Create machine task - Bench.im`;
-    }
-  });
-
-  let isEdit = false;
-  const pk = searchParams.get("pk")
-  if (pk) {
-    const [changed, setChanged] = useState(false);
-    const { data, error } = useSWR(`/api/machine_task/?pk=${pk}&edit=1`);
-    if (error || !data) {
-      return (
-        <div></div>
-      )
-    }
-    if (!changed) {
-      setMachineId(data.machine_id);
-      setState(data.state);
-      setOneshot(data.oneshot)
-      setIpv6(data.detail.ipv6)
-      setThread(data.detail.ipv6)
-      setServerId(data.detail.server)
-      setChanged(true);
-    }
-    isEdit = true;
-  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const data = {
       "machine_id": machineId,
-      "state": state,
-      "oneshot": oneshot,
       "ipv6": ipv6,
       "thread": thread,
       "server_id": serverId,
-    }
-    if (pk) {
-      data.pk = pk;
     }
     try {
       const r = await fetch("/api/machine_task/", {
@@ -95,7 +43,8 @@ const MachineTaskCreate = () => {
       }
       const res = await r.json();
       const pk = res.pk;
-      navigate(`/machine/${uuid}/`);
+      setShow(false)
+      mutate(`/api/machine/?pk=${uuid}`);
     }
     catch (err) {
       alert("Network Error! Please refresh the page and try again.")
@@ -103,30 +52,16 @@ const MachineTaskCreate = () => {
   }
 
   return (
-    <div>
-      <div className="mx-auto sm:w-2/5 py-2 text-justify">
-        {isEdit ? <p className="text-justify"><Link className="underline" to="/my/"><FormattedMessage defaultMessage="You" /></Link><FormattedMessage defaultMessage=" are editing machine task" /> <span className="before:content-['#'] px-1 mr-2 bg-stone-700 text-white">{pk}</span></p> : ""}
-      </div>
-      <div className="mx-auto sm:w-2/5 text-justify leading-8">
-        <form onSubmit={handleSubmit}>
-          <p>Machine ID: {machineId}</p>
-          <label><FormattedMessage defaultMessage="State:" /><br /></label>
-          <select className="w-full" value={state} onChange={(e)=> {setState(e.target.value)}}>
-            <option value="Wait">Wait</option>
-            <option value="Work">Work</option>
-            <option value="Finish">Finish</option>
-          </select>
-          <label><FormattedMessage defaultMessage="Server id:" /><br /></label>
-          <input className="w-full" type="text" value={serverId} onChange={(e) => setServerId(e.target.value)} />
-          <label><FormattedMessage defaultMessage="Theads:" /><br /></label>
-          <input className="w-full" type="text" value={thread} onChange={(e) => setThread(e.target.value)} />
-          <label><FormattedMessage defaultMessage="Oneshot:" /></label>
-          <input className="mx-2" type="checkbox" checked={oneshot} onChange={(e) => setOneshot(!oneshot)} />
-          <label><FormattedMessage defaultMessage="IPv6:" /></label>
-          <input className="mx-2" type="checkbox" checked={ipv6} onChange={(e) => setIpv6(!ipv6)} />
-          <button className="w-full border border-gray-700 bg-white my-2 py-1" type="submit"><FormattedMessage defaultMessage="Submit" /></button>
-        </form>
-      </div>
+    <div className="my-2">
+      <form onSubmit={handleSubmit}>
+        <label><FormattedMessage defaultMessage="Server id:" /><br /></label>
+        <input className="w-full" type="text" value={serverId} onChange={(e) => setServerId(e.target.value)} />
+        <label><FormattedMessage defaultMessage="Theads:" /><br /></label>
+        <input className="w-full" type="text" value={thread} onChange={(e) => setThread(e.target.value)} />
+        <label><FormattedMessage defaultMessage="IPv6:" /></label>
+        <input className="mx-2" type="checkbox" checked={ipv6} onChange={(e) => setIpv6(!ipv6)} />
+        <button className="w-full border border-gray-700 bg-white my-2 py-1.5" type="submit"><FormattedMessage defaultMessage="Submit" /></button>
+      </form>
     </div>
 
   )
