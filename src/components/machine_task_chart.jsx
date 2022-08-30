69559a592a518dbe6066602d4d99d7861d859882
veoco <one@nomox.cn>
2022-08-30 08:34:01 +0800

    change to LineAdvance

--- src/components/machine_task_chart.jsx
+++ src/components/machine_task_chart.jsx
@@ -1,8 +1,8 @@
-import { Chart, Geom, Axis } from "bizcharts";
+import { Chart, Geom, Axis, LineAdvance } from "bizcharts";
 import DataSet from '@antv/data-set';
 
 
-const MachineTaskChart = ({item, name}) => {
+const MachineTaskChart = ({ item, name }) => {
   const data = [];
   const fmt = {
     "3h": "HH:mm",
@@ -10,6 +10,8 @@ const MachineTaskChart = ({item, name}) => {
     "10d": "MM/DD",
     "360d": "YY MM/DD"
   }
+  let speed_max = 100;
+  let ping_max = 300;
   for (const row of item[name]) {
     const hour = new Date(row[0] * 1000);
     const upload = row[1];
@@ -19,12 +21,17 @@ const MachineTaskChart = ({item, name}) => {
     const r = { "hour": hour };
     if (upload) {
       r.upload = upload;
+      speed_max = speed_max < 1000 && upload > 100 ? 1000 : speed_max;
+      speed_max = speed_max < 10000 && upload > 1000 ? 10000 : speed_max;
     }
     if (download) {
       r.download = download;
+      speed_max = speed_max < 1000 && download > 100 ? 1000 : speed_max;
+      speed_max = speed_max < 10000 && download > 1000 ? 10000: speed_max;
     }
     if (ping) {
       r.ping = ping;
+      ping_max = ping_max < 1000 && ping > 300 ? 1000 : ping_max;
     }
     data.push(r);
   }
@@ -37,7 +44,7 @@ const MachineTaskChart = ({item, name}) => {
     value: "value"
   });
 
-  const scale_speed = {
+  const scale = {
     value: {
       alias: "Speed",
       type: 'linear-strict',
@@ -46,6 +53,7 @@ const MachineTaskChart = ({item, name}) => {
       },
       nice: true,
       tickCount: 5,
+      max: speed_max,
       min: 0
     },
     ping: {
@@ -55,6 +63,7 @@ const MachineTaskChart = ({item, name}) => {
         return val + ' ms';
       },
       tickCount: 5,
+      max: ping_max,
       min: 0
     },
     hour: {
@@ -65,13 +74,30 @@ const MachineTaskChart = ({item, name}) => {
     },
   };
 
+  const axisConfig = {
+    line: {
+      style: {
+        stroke: '#ccc',
+        lineDash: [3, 3],
+      }
+    },
+    grid: {
+      line: {
+        style: {
+          stroke: '#ccc',
+          lineDash: [3, 3],
+        },
+      }
+    },
+  }
+
   return (
-    <Chart height={200} data={dv.rows} scale={scale_speed} autoFit>
-      <Axis name="hour" />
-      <Axis name="value" />
-      <Axis name="ping" />
-      <Geom type="interval" position="hour*value" color={["key", ["skyblue", "lightcoral"]]} />
-      <Geom type="line" position="hour*ping" size={1} color={"green"} shape={"hv"} />
+    <Chart className="border border-gray-700 p-2 pt-2 pb-0 mt-2" height={200} data={dv.rows} scale={scale} autoFit>
+      <Axis name="hour" {...axisConfig} />
+      <Axis name="value" {...axisConfig} />
+      <Axis name="ping" {...axisConfig} />
+      <LineAdvance type="interval" position="hour*value" color={["key", ["skyblue", "lightcoral"]]} area />
+      <LineAdvance type="line" position="hour*ping" size={1} color={"green"} shape={"hv"} />
     </Chart>
   )
 }
