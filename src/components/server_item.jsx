e490143c0a0af834a59c5e79efdf68aee00847fd
veoco <one@nomox.cn>
2022-09-01 18:49:22 +0800

    change id color

--- src/components/server_item.jsx
+++ src/components/server_item.jsx
@@ -66,14 +66,14 @@ const ServerItem = ({ item, isEdit, index, serverDict, setServerDict }) => {
   if (isEdit) {
     return (
       <div className="my-2 border border-gray-700 bg-white p-2 group last:mb-0 first:mt-0" draggable onDragStart={handleDragStart} onDragOver={handleDragOver} onDrop={handleDragDrop}>
-        <h3><span className="before:content-['#'] px-1 mr-2 bg-stone-200 text-gray-700">{item.pk}</span>{name}</h3>
+        <h3><span className="before:content-['#'] px-1 mr-2 bg-sky-700 text-white">{item.pk}</span>{name}</h3>
         <p className="text-gray-400 text-justify">{item.provider} - {item.detail.id}{cc} - {item.detail.ipv6?"IPv6 · ":""}{host}{isEdit ? deleteButton : ""}</p>
       </div>
     )
   }
   return (
     <div className="my-2 border border-gray-700 bg-white p-2 group last:mb-0 first:mt-0">
-      <h3><span className="before:content-['#'] px-1 mr-2 bg-stone-200 text-gray-700">{item.pk}</span><Link to={`/server/${item.pk}/`}>{name}</Link></h3>
+      <h3><span className="before:content-['#'] px-1 mr-2 bg-sky-700 text-white">{item.pk}</span><Link to={`/server/${item.pk}/`}>{name}</Link></h3>
       <p className="text-gray-400 text-justify">{item.provider} - {item.detail.id}{cc} - {item.detail.ipv6?"IPv6 · ":""}{host}</p>
     </div>
   )
--- src/components/server_list_item.jsx
+++ src/components/server_list_item.jsx
@@ -7,7 +7,7 @@ const ServerListItem = ({ item }) => {
 
   return (
     <div className="my-2 border border-gray-700 bg-white p-2">
-      <h3><span className="before:content-['#'] px-1 mr-2 bg-stone-700 text-white">{item.pk}</span><Link to={`/server_list/${item.pk}/`}>{item.name}</Link></h3>
+      <h3><span className="before:content-['#'] px-1 mr-2 bg-purple-700 text-white">{item.pk}</span><Link to={`/server_list/${item.pk}/`}>{item.name}</Link></h3>
       <div className="text-gray-400">
         {item.owner ? <span className="mr-2">{item.owner.username}</span> : ""}
         <span>{created == modified ? created.toLocaleString() : modified.toLocaleString()}</span>
