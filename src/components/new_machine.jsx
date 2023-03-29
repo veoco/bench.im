dff8e7a763f1239537b15c85e34cd1dd357847b1
veoco <one@nomox.cn>
2023-03-29 14:40:38 +0800

    新增其他版本链接

--- src/components/new_machine.jsx
+++ src/components/new_machine.jsx
@@ -8,7 +8,7 @@ export default function NewMachineBlock({ token }) {
       <h3 className="font-bold pb-2 border-b">增加测速节点</h3>
       <p className='my-2'>节点名称（16 字符以内，留空自动使用 IP 地址）：</p>
       <input className='px-2 py-1 shadow w-full' type="text" value={name} placeholder={"输入名称"} onChange={(e) => setName(e.target.value)} />
-      <p className='my-2'>测试使用：</p>
+      <p className='my-2'>测试使用（默认 x86_64，<a className='underline' href="https://github.com/veoco/bim" target="_blank">其他</a>）：</p>
       <div className="bg-gray-100 p-2 font-mono">
         <p>wget https://bench.im/dl/bimc -O bimc</p>
         <p>chmod +x bimc</p>
--- src/components/new_server.jsx
+++ src/components/new_server.jsx
@@ -8,7 +8,7 @@ export default function NewServerBlock({ token }) {
       <h3 className="font-bold pb-2 border-b">增加目标服务器</h3>
       <p className='my-2'>服务器名称（16 字符以内，留空自动使用 IP 地址）：</p>
       <input className='px-2 py-1 shadow w-full' type="text" value={name} placeholder={"输入名称"} onChange={(e) => setName(e.target.value)} />
-      <p className='my-2'>测试使用：</p>
+      <p className='my-2'>测试使用（默认 x86_64，<a className='underline' href="https://github.com/veoco/bim" target="_blank">其他</a>）：</p>
       <div className="bg-gray-100 p-2 font-mono">
         <p>wget https://bench.im/dl/bim -O bim</p>
         <p>chmod +x bim</p>
