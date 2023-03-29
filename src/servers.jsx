08f1abf57904f2ff121e99624f1884e58ec5c2b7
veoco <one@nomox.cn>
2023-03-29 18:45:11 +0800

    修复名称过长导致的创建失败

--- src/servers.jsx
+++ src/servers.jsx
@@ -1,67 +1,67 @@
 import { useState } from 'react'
 
 const serversList = {
-  "V.PS, Amsterdam, Netherlands": {
+  "V.PS AMS": {
     "upload_url": "https://ams.speedtest.v.ps/empty.php",
     "download_url": "https://ams.speedtest.v.ps/garbage.php"
   },
-  "V.PS, Amsterdam, Netherlands, Nano": {
+  "V.PS AMS Nano": {
     "upload_url": "https://nano-ams.speedtest.v.ps/empty.php",
     "download_url": "https://nano-ams.speedtest.v.ps/garbage.php"
   },
-  "V.PS, Amsterdam, Netherlands, Storage": {
+  "V.PS AMS Storage": {
     "upload_url": "https://storage-ams.speedtest.v.ps/empty.php",
     "download_url": "https://storage-ams.speedtest.v.ps/garbage.php"
   },
-  "V.PS, Frankfurt, Germany": {
+  "V.PS FRA": {
     "upload_url": "https://fra.speedtest.v.ps/empty.php",
     "download_url": "https://fra.speedtest.v.ps/garbage.php"
   },
-  "V.PS, Frankfurt, Germany, Nano": {
+  "V.PS FRA Nano": {
     "upload_url": "https://nano-fra.speedtest.v.ps/empty.php",
     "download_url": "https://nano-fra.speedtest.v.ps/garbage.php"
   },
-  "V.PS, Düsseldorf, Germany": {
+  "V.PS DUS": {
     "upload_url": "https://dus.speedtest.v.ps/empty.php",
     "download_url": "https://dus.speedtest.v.ps/garbage.php"
   },
-  "V.PS, Tallinn, Estonia": {
+  "V.PS TLL": {
     "upload_url": "https://tll.speedtest.v.ps/empty.php",
     "download_url": "https://tll.speedtest.v.ps/garbage.php"
   },
-  "V.PS, Seattle, United States": {
+  "V.PS SEA": {
     "upload_url": "https://sea.speedtest.v.ps/empty.php",
     "download_url": "https://sea.speedtest.v.ps/garbage.php"
   },
-  "V.PS, Seattle 2, United States": {
+  "V.PS SEA2": {
     "upload_url": "https://sea2.speedtest.v.ps/empty.php",
     "download_url": "https://sea2.speedtest.v.ps/garbage.php"
   },
-  "V.PS, New York, United States": {
+  "V.PS NYC": {
     "upload_url": "https://nyc.speedtest.v.ps/empty.php",
     "download_url": "https://nyc.speedtest.v.ps/garbage.php"
   },
-  "V.PS, Hong Kong, China": {
+  "V.PS HKG": {
     "upload_url": "https://hkg.speedtest.v.ps/empty.php",
     "download_url": "https://hkg.speedtest.v.ps/garbage.php"
   },
-  "V.PS, London, United Kingdom": {
+  "V.PS LON": {
     "upload_url": "https://lon.speedtest.v.ps/empty.php",
     "download_url": "https://lon.speedtest.v.ps/garbage.php"
   },
-  "V.PS, Osaka, Japan": {
+  "V.PS KIX": {
     "upload_url": "https://kix.speedtest.v.ps/empty.php",
     "download_url": "https://kix.speedtest.v.ps/garbage.php"
   },
-  "V.PS, San Jose, United States": {
+  "V.PS SJC": {
     "upload_url": "https://sjc.speedtest.v.ps/empty.php",
     "download_url": "https://sjc.speedtest.v.ps/garbage.php"
   },
-  "V.PS, Tokyo, Japan": {
+  "V.PS NRT": {
     "upload_url": "https://nrt.speedtest.v.ps/empty.php",
     "download_url": "https://nrt.speedtest.v.ps/garbage.php"
   },
-  "V.PS, Sydney, Australia": {
+  "V.PS SYD": {
     "upload_url": "https://syd.speedtest.v.ps/empty.php",
     "download_url": "https://syd.speedtest.v.ps/garbage.php"
   },
