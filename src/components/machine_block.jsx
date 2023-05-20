f60db7805db47faf502ef7fc59b8d256c6207340
veoco <one@nomox.cn>
2023-05-20 20:02:52 +0800

    调整适配 nickname

--- src/components/machine_block.jsx
+++ src/components/machine_block.jsx
@@ -5,7 +5,7 @@ export default function MachineBlock({ item }) {
 
   return (
     <div className='flex flex-col cursor-pointer bg-white shadow rounded p-3' onClick={() => setLocation(`/m/${item.id}`)}>
-      <h3 className="font-bold mb-1">{item.name}</h3>
+      <h3 className="font-bold mb-1">{item.nickname}</h3>
       <p className="text-xs text-gray-500">{item.ip}</p>
     </div>
   )
--- src/machine_page.jsx
+++ src/machine_page.jsx
@@ -15,10 +15,15 @@ export default function MachinePage({ params }) {
     <p>加载中</p>
   </div>
 
+  const ipv4s = data.targets.filter(obj => obj.ipv6 === false);
+  ipv4s.sort((a, b) => a.name.localeCompare(b.name));
+  const ipv6s = data.targets.filter(obj => obj.ipv6 === true);
+  ipv6s.sort((a, b) => a.name.localeCompare(b.name));
+
   return (
     <div className='mt-2'>
       <div className="flex items-baseline">
-        <h2 className='font-bold text-2xl mr-2'>{data.detail.name}</h2>
+        <h2 className='font-bold text-2xl mr-2'>{data.detail.nickname}</h2>
         <p className="text-gray-500 text-sm">{data.detail.ip}</p>
       </div>
       <div className="flex mt-2 text-sm">
@@ -28,14 +33,23 @@ export default function MachinePage({ params }) {
           <option value="7d">最近 7 天</option>
         </select>
       </div>
+      <h3 className="mt-2 p-2 border-b font-bold border-gray-600">IPv4</h3>
       <div className='mt-3 gap-2 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3'>
-        {data.targets.map((item) => {
+        {ipv4s.map((item) => {
           return (
             <div className='bg-white border shadow rounded p-2' key={item.id}>
               <h3 className='font-bold text-lg'>{item.name}</h3>
-              <ul className="flex text-xs text-gray-400 items-center mb-1">
-                <li className="before:content-['#'] my-1 mr-1">{item.ipv6 ? "IPv6" : "IPv4"}</li>
-              </ul>
+              <TcpPingBlock mid={params.mid} tid={item.id} fixedY={fixedY} dateRange={dateRange} />
+            </div>
+          )
+        })}
+      </div>
+      <h3 className="mt-2 p-2 border-b font-bold border-gray-600">IPv6</h3>
+      <div className='mt-3 gap-2 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3'>
+        {ipv6s.map((item) => {
+          return (
+            <div className='bg-white border shadow rounded p-2' key={item.id}>
+              <h3 className='font-bold text-lg'>{item.name}</h3>
               <TcpPingBlock mid={params.mid} tid={item.id} fixedY={fixedY} dateRange={dateRange} />
             </div>
           )
