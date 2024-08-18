2c28cb5aaffc174b66b5e14a10bc5612df7d3a49
veoco <one@nomox.cn>
2024-08-18 13:44:36 +0800

    调整 post 为 get

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <SWRConfig value={{
-      fetcher: (resource, init) => fetch(resource, { method: "POST", ...init }).then(res => res.json()),
+      fetcher: (resource, init) => fetch(resource, init).then(res => res.json()),
       refreshInterval: 100000
     }}>
       <App />
