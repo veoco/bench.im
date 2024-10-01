ff4056346f009be96250fd0410575c44a6acec96
veoco <one@nomox.cn>
2024-10-01 20:44:34 +0800

    调整 api 目标

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,11 @@ export default defineConfig({
   server: {
     host: "127.0.0.1",
     proxy: {
-      '/api': 'http://127.0.0.1:8000',
+      // '/api': 'http://127.0.0.1:8000',
+      '/api': {
+        target: 'https://bench.im',
+        changeOrigin: true,
+      },
     }
   }
-
 })
