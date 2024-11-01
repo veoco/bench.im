2ecc0ea42a8cc8c2118bc36b51aa0d8e60a02e73
veoco <one@nomox.cn>
2024-11-01 16:25:39 +0800

    调整开发服务器

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     host: "127.0.0.1",
     proxy: {
-      '/api': 'http://127.0.0.1:3000',
-      // '/api': {
-      //   target: 'https://bench.im',
-      //   changeOrigin: true,
-      // },
+      // '/api': 'http://127.0.0.1:3000',
+      '/api': {
+        target: 'https://bench.im',
+        changeOrigin: true,
+      },
     }
   }
 })
