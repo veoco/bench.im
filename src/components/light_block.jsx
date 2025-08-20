459d28c429a31137d0b04d21a4b4ce5f019d7c88
veoco <one@nomox.cn>
2025-08-20 17:23:47 +0800

    修复时间计算

--- src/components/light_block.jsx
+++ src/components/light_block.jsx
@@ -1,10 +1,12 @@
 export default function LightBlock({ updated }) {
+  const currentTime = Date.now();
+  const updatedTime = updated ? new Date(updated * 1000).getTime() : null;
   return (
     <div className="flex items-center ml-auto">
       <span className={`w-2 h-2 rounded-full mr-2 ${!updated ? 'bg-gray-400' :
-        (Date.now() - new Date(updated).getTime() < 5 * 60 * 1000 ? 'bg-green-500' :
-          (Date.now() - new Date(updated).getTime() < 10 * 60 * 1000 ? 'bg-yellow-500' : 'bg-red-500'))}`}
-        title={updated ? `最后更新: ${new Date(updated).toLocaleString()}` : '未上线'}>
+        (currentTime - updatedTime < 5 * 60 * 1000 ? 'bg-green-500' :
+          (currentTime - updatedTime < 10 * 60 * 1000 ? 'bg-yellow-500' : 'bg-red-500'))}`}
+        title={updated ? `最后更新: ${new Date(updatedTime).toLocaleString()}` : '未上线'}>
       </span>
     </div>
   )
